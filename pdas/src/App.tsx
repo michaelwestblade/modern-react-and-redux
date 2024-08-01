5764c44e5eaab61596825d260668624e5f7c05d2
michaelwestblade <michaelwestblade@gmail.com>
2024-08-01 14:11:37 -0500

    cards with bulma

---
+++ pdas/src/App.tsx
@@ -0,0 +1,33 @@
+import 'bulma/css/bulma.min.css';
+import React from 'react';
+import './App.css';
+import ProfileCard from "./ProfileCard";
+import AlexaImage from './images/alexa.png'
+import CortanaImage from './images/cortana.png'
+import SiriImage from './images/siri.png'
+
+function App() {
+  return (
+    <div className="App">
+        <div>Personal Digital Assistants</div>
+
+        <div className="container">
+            <section className="section">
+                <div className="columns">
+                    <div className="column is-4">
+                        <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage}/>
+                    </div>
+                    <div className="column is-4">
+                        <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage}/>
+                    </div>
+                    <div className="column is-4">
+                        <ProfileCard title="Siri" handle="@siri01" image={SiriImage}/>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ pdas/src/ProfileCard.tsx
@@ -0,0 +1,30 @@
+interface ProfileCardProps {
+    title: string;
+    handle: string;
+    image: string;
+}
+
+const ProfileCard = ({title, handle, image}: ProfileCardProps) => {
+    return <div className="card">
+        <div className="card-image">
+            <figure className="image is-1by1">
+                <img src={image} alt={`${title} image`}/>
+            </figure>
+        </div>
+        <div className="card-content">
+            <div className="media">
+                <div className="media-left">
+                    <figure className="image is-48x48">
+                        <img src={image} alt={`${title} image`}/>
+                    </figure>
+                </div>
+                <div className="media-content">
+                    <p className="title is-4">{title}</p>
+                    <p className="subtitle is-6">{handle}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default ProfileCard;
